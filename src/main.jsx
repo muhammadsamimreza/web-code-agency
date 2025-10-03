6d552f9bb57485ee52802b60498f1cefcf17498e
muhammad samim reza <muhammadsamimreza@gmail.com>
2025-10-03 22:47:07 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,37 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter } from 'react-router'
+import { RouterProvider } from 'react-router'
+import Root from './assets/Root'
+import Home from './pages/Home/Home'
+import Service from './pages/Service/Service'
+import Blogs from './pages/Blogs/Blogs'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    Component: Root,
+    children: [
+      {
+        path: '/home',
+        Component: Home
+      },
+      {
+        path: '/service',
+        Component: Service
+      },
+      {
+        path: '/blogs',
+        Component: Blogs
+      },
+    ]
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>
+)
